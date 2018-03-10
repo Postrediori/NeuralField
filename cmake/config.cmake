f68ff1952db63f7327f380ec67870294934e377d
Rasim Labibov <postrediori@gmail.com>
2018-03-10 12:43:14 +0200

    Treat warnings as errors

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -10,7 +10,7 @@ macro(make_project_)
     if (MSVC)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W4")
     else ()
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -std=c++11")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -Werror -std=c++11")
     endif ()
     
     if (MSVC)
